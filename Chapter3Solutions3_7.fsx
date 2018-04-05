16f496bf3ed523dd7bd6d5b9289233a8057cdb84
Yuval Khachatryan <37039987+yuval-khachatryan@users.noreply.github.com>
2018-04-05 13:29:13 +0300

    Solutions to chapter 3
    Solutions for 1 and 6 are not complete - but it is obvious what to do from there.

---
+++ Chapter3Solutions3_7.fsx
@@ -0,0 +1,70 @@
+﻿(* Solutions for chapter 3 - exercise 3 to 7 *)
+
+(* Exercise 3 - Complex numbers *)
+type Complex = {Real: float; Imaginary: float}
+
+// addition and multiplication
+let (.+.) z1 z2 = {Real = z1.Real + z2.Real; Imaginary = z1.Imaginary + z2.Imaginary}
+let (.*.) z1 z2 = {Real = z1.Real * z2.Real - z1.Imaginary * z2.Imaginary;
+                   Imaginary = z1.Real * z2.Imaginary + z1.Imaginary * z2.Real}
+
+
+// inverse operators
+// additive inverse
+let (~-.) z = {Real = -z.Real; Imaginary = -z.Imaginary}
+
+let inverse z = 
+    let normSquared = z.Real * z.Real + z.Imaginary * z.Imaginary
+    {Real = z.Real / normSquared; Imaginary = -z.Imaginary / normSquared}
+
+// substraction and division
+let (.-.) z1 z2 = z1 .+. (-. z2)
+let (./.) z1 z2 = z1 .*. (inverse z2)
+
+// exercise 4 - stright line ax + b
+// declares a function to mirror stright line across x and y axes
+// and representation of stright line as a string
+type StrigthLine = float * float
+
+let mirrorX (a, b) = (-a, -b)
+let mirrorY (a, b) = (-a, b)
+let toString (a, b) = (string a) + "x + " + (string b)
+
+// Exercise 5 - Solves quadratic equation with solution having a type that captures possibilities for solutioins
+// namely: no roots, one root, two roots and corresponding solve function
+// Solution of quadratic eqation caputuring the options
+type Solution = TwoRoots of float * float | OneRoot of float | NoRoots
+type QuadraticEquation = float * float * float
+
+let solve (a, b, c) = 
+    let disc = b * b - 4.0 * a * c
+    match disc with
+    | 0.0 -> OneRoot (-b / (2.0 * a) )
+    | _ when disc > 0.0 -> TwoRoots ( (-b + System.Math.Sqrt disc) / (2.0 * a), (-b - System.Math.Sqrt disc) / (2.0 * a)  ) 
+    | _ -> NoRoots
+
+/// Exercise 6 - Sort of has been done... Look at exercise 1,2 solutions
+
+/// Exercise 7
+type Shape = | Circle of float
+             | Square of float
+             | Triangle of float * float * float
+
+let isShape s =
+    match s with
+    | Circle r -> r > 0.0
+    | Square s -> s > 0.0
+    | Triangle (a,b,c) ->
+        a > 0.0 && b > 0.0 && c > 0.0 
+        && a < b + c && b < a + c && c < a + b
+
+let area = 
+    function
+    | Circle r when r > 0.0 -> System.Math.PI * r * r
+    | Square s when s > 0.0 -> s * s
+    | Triangle (a, b, c) 
+        when a > 0.0 && b > 0.0 && c > 0.0 
+        && a < b + c && b < a + c && c < a + b ->
+            let s = (a + b + c) / 2.0
+            sqrt (s * (s - a) * (s - b) * (s - c))
+    | _ -> failwith "not a legal shape"
---
+++ Chapter3SolutionsEx1_2.fsx
@@ -0,0 +1,71 @@
+﻿(* Solutions for chapter 3 - this is exercise 1 and 2 - the files is split to avoid new unnecessary operators*)
+
+// Exercise 1 - tuple version
+type TimeTuple = int * int * string
+
+// checks whether we a legal time
+let isLegal (hh, mm, ampm) =
+    (1 <= hh && hh <= 12) && (0 <= mm && mm <= 59) && (ampm = "AM" || ampm = "PM")
+
+// checkes whether first time1 comes before time2
+// assumes we have valid times already
+let (.<.) time1 time2 = 
+    match (time1, time2) with
+    | (_, _) when (not (isLegal time1) ) || (not (isLegal time2) ) -> failwith "Illegal time argument"
+    | (_, _, "AM"), (_, _, "PM") -> true
+    | (_, _, "PM"), (_, _, "AM") -> false
+    | (h1, m1, _), (h2, m2, _) -> (h1 % 12 < h2 % 12) || (h1 = h2 && m1 < m2)
+
+// record version
+type Meridem = AM | PM
+type TimeRecord = {Hour : int; Minute: int; AmPm : Meridem}
+
+let legalTime time = 
+    (1 <= time.Hour && time.Hour <= 12) && (0 <= time.Minute && time.Minute <= 59) 
+
+let ( <. ) time1 time2 = 
+    time1.AmPm < time2.AmPm 
+    || (time1.AmPm = time2.AmPm && time1.Hour % 12 < time2.Hour % 12)
+    || (time1.AmPm = time2.AmPm && time1.Hour = time2.Hour && time1.Minute = time2.Minute)
+
+(* Exercise 2: The former British currency had 12 pence to a shilling and 20 shillings to a pound. Declare
+functions to add and subtract two amounts, represented by triples (pounds, shillings, pence) of
+integers, and declare the functions when a representation by records is used. Declare the func-
+tions in infix notation with proper precedences, and use patterns to obtain readable declarations. *)
+
+//  currency expression as tuple
+type MoneyTuple = int * int * int
+
+// convert to pounds
+let tupleMoneyToPence (pounds, shillings, pence) =
+    pounds * 240 + shillings * 12 + pence
+
+// convert back
+let penceToMoneyTuple coins = 
+    let pounds = coins / 240
+    let shillings = (coins % 240) / 12
+    let pence = coins % 12
+    (pounds, shillings, pence)
+
+// we transform values to some other type then apply the operator and then transfrom them back
+// it is usually assumed that convert and convertBack are inverses of one of other and that 
+// convert : 'a -> 'b where 'a and 'b are isomorphic
+let combine operator convert convertBack value1 value2 = 
+    operator (convert value1) (convert value2) |> convertBack
+
+let (.++.) = combine (+) tupleMoneyToPence penceToMoneyTuple
+let (.--.) = combine (-) tupleMoneyToPence penceToMoneyTuple
+
+type MoneyRecord = {Pounds: int; Shillings: int; Pence: int}
+
+let recordMoneyToPence money = 
+    money.Pounds * 240 + money.Shillings * 12 + money.Pence
+
+let penceToMoneyRecord coins = 
+    let pounds = coins / 240
+    let shillings = (coins % 240) / 12
+    let pence = coins % 12
+    {Pounds = pounds; Shillings = shillings; Pence = pence}
+
+let (..++..) = combine (+) recordMoneyToPence penceToMoneyRecord
+let (..--..) = combine (-) recordMoneyToPence penceToMoneyRecord
